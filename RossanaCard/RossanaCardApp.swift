89c14394454f52db969fe3bd9c240cb6eaa791b5
Rossana Laricchia <rossana.laricchia@eng.it>
2022-04-04 12:09:18 +0200

    Initial Commit

---
+++ RossanaCard/RossanaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RossanaCardApp.swift
+//  RossanaCard
+//
+//  Created by Rossana Laricchia on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RossanaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
